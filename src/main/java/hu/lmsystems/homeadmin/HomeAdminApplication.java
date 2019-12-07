ba048b8609d2f62f994cf2f8972bbc3a9ae2fb4c
Lehel Mezei <mezei.lehel@webtown.hu>
2019-12-07 09:08:10 +0100

    Initialized project.

---
+++ src/main/java/hu/lmsystems/homeadmin/HomeAdminApplication.java
@@ -0,0 +1,13 @@
+package hu.lmsystems.homeadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/lmsystems/homeadmin/HomeAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.lmsystems.homeadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
