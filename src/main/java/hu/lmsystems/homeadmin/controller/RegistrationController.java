fa9d20a2a617ca1ad7c6ca207e3ac70ff8d604f1
Lehel Mezei <mezei.lehel@webtown.hu>
2019-12-10 09:51:09 +0100

    TK-12: spring security dependencies added, doesn't working yet.

--- src/main/java/hu/lmsystems/homeadmin/controller/RegistrationController.java
+++ src/main/java/hu/lmsystems/homeadmin/controller/RegistrationController.java
@@ -1,11 +1,5 @@
 package hu.lmsystems.homeadmin.controller;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-
-import javax.validation.Constraint;
-import javax.validation.Payload;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
