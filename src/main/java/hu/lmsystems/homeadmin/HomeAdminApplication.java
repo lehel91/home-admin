4f85b3b391909b6b97726b37646f34da70844794
Lehel <mezei.lehel@webtown.hu>
2020-02-20 16:54:12 +0100

    JPA authentication finished, works with mysql db.

--- src/main/java/hu/lmsystems/homeadmin/HomeAdminApplication.java
+++ src/main/java/hu/lmsystems/homeadmin/HomeAdminApplication.java
@@ -2,8 +2,10 @@ package hu.lmsystems.homeadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class HomeAdminApplication {
 
 	public static void main(String[] args) {
